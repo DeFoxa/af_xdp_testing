89274c45cd20a6cb2c7863d9eae3ea3e416c9dc6
DeFoxa <113225642+DeFoxa@users.noreply.github.com>
2024-08-12 14:19:24 -0500

    remove old imports

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,4 @@
 #![allow(warnings)]
-use std::{net::UdpSocket, time::Instant};
-// use xsk_rs::{Config, Socket, Umem};
-use std::sync::atomic::{AtomicBool, Ordering};
-use structopt::StructOpt;
-use xsk_rs::{
-    config::{BindFlags, FrameSize, Interface, QueueSize, SocketConfig, UmemConfig},
-    CompQueue, FillQueue, FrameDesc, RxQueue, Socket, TxQueue, Umem,
-};
-
 pub mod af_xdp;
 pub mod metrics;
 pub mod udp;
