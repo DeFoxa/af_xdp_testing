300ff8f6a5a060d1ea00b77237f005b86d083512
DeFoxa <113225642+DeFoxa@users.noreply.github.com>
2024-08-15 15:43:50 -0500

    cleanup old code

--- crates/af_xdp/src/bin/test_xdp_binary.rs
+++ crates/af_xdp/src/bin/test_xdp_binary.rs
@@ -6,6 +6,7 @@ use aya::{
     Bpf,
 };
 use aya_log::BpfLogger;
+use env_logger::{Builder, Env};
 use eyre::Result;
 use log::info;
 use tokio::signal;
@@ -13,13 +14,10 @@ use tokio::signal;
 #[tokio::main]
 async fn main() -> Result<()> {
     env_logger::init();
-    println!("program running");
 
-    // #[cfg(debug_assertions)]
     let mut bpf = Bpf::load(include_bytes_aligned!(
         "../../../../target/bpfel-unknown-none/release/xdp-trace-ebpf"
     ))?;
-    // let bpf_path = std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("")
 
     if let Err(e) = BpfLogger::init(&mut bpf) {
         log::warn!("failure to initialize eBPF logger {}", e);
@@ -34,4 +32,8 @@ async fn main() -> Result<()> {
     info!("exiting");
 
     Ok(())
+}
+
+fn setup_loggin() {
+    Builder::from_env(Env::default().default_filter_or("info")).init();
 }
