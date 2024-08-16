96b28b16c745bc77bc9dcf115ca485e63d3c9e73
DeFoxa <113225642+DeFoxa@users.noreply.github.com>
2024-08-16 13:30:20 -0500

    simplify try_xdp_tracer

--- crates/xdp-trace-ebpf/src/main.rs
+++ crates/xdp-trace-ebpf/src/main.rs
@@ -29,73 +29,71 @@ static mut EVENTS: PerfEventArray<XdpEvent> = PerfEventArray::new(0);
 
 #[xdp]
 pub fn xdp_tracer(ctx: XdpContext) -> u32 {
-    match try_xdp_tracer(ctx) {
-        Ok(ret) => ret,
+    match try_xdp_tracer(&ctx) {
+        Ok(action) => action,
         Err(_) => xdp_action::XDP_ABORTED,
     }
 }
+#[inline(always)]
+fn try_xdp_tracer(ctx: &XdpContext) -> Result<u32, u32> {
+    let eth = ptr_at::<EthHdr>(ctx, 0)?;
+    if unsafe { (*eth).ether_type } != EtherType::Ipv4 {
+        return Ok(xdp_action::XDP_PASS);
+    }
 
-fn try_xdp_tracer(ctx: XdpContext) -> Result<u32, u32> {
-    if should_process_packet(&ctx) {
+    let ipv4 = ptr_at::<Ipv4Hdr>(ctx, EthHdr::LEN)?;
+    if unsafe { (*ipv4).proto } != IpProto::Udp {
+        return Ok(xdp_action::XDP_PASS);
+    }
+
+    let udp = ptr_at::<UdpHdr>(ctx, EthHdr::LEN + Ipv4Hdr::LEN)?;
+    let dst_port = u16::from_be(unsafe { (*udp).dest });
+    let src_port = u16::from_be(unsafe { (*udp).source });
+
+    if src_port == 7777 || dst_port == 7777 {
         let event = XdpEvent {
             timestamp: unsafe { bpf_ktime_get_ns() },
             packet_size: (ctx.data_end() - ctx.data()) as u32,
             action: xdp_action::XDP_PASS,
         };
-
         unsafe {
-            EVENTS.output(&ctx, &event, 0);
+            EVENTS.output(ctx, &event, 0);
         }
-
-        info!(&ctx, "XDP event logged");
+        info!(ctx, "Packet on port 7777 detected");
     }
 
     Ok(xdp_action::XDP_PASS)
 }
 
-fn should_process_packet(ctx: &XdpContext) -> bool {
-    match try_should_process_packet(ctx) {
-        Ok(should_process) => should_process,
-        Err(_) => false,
-    }
-}
-fn try_should_process_packet(ctx: &XdpContext) -> Result<bool, ()> {
-    let eth = unsafe { ptr_at::<EthHdr>(ctx, 0) }?;
-
-    let ether_type = unsafe { ptr::read_unaligned(ptr::addr_of!((*eth).ether_type)) };
-
-    if ether_type != EtherType::Ipv4 {
+fn should_process_packet(ctx: &XdpContext) -> Result<bool, u32> {
+    let eth = ptr_at::<EthHdr>(ctx, 0).unwrap();
+    if unsafe { (*eth).ether_type } != EtherType::Ipv4 {
         return Ok(false);
     }
 
-    let ip = unsafe { ptr_at::<Ipv4Hdr>(ctx, EthHdr::LEN) }?;
-
-    if ip.proto != IpProto::Udp {
+    let ipv4 = ptr_at::<Ipv4Hdr>(ctx, EthHdr::LEN).unwrap();
+    if unsafe { (*ipv4).proto } != IpProto::Udp {
         return Ok(false);
     }
 
-    let udp = unsafe { ptr_at::<UdpHdr>(ctx, EthHdr::LEN + Ipv4Hdr::LEN) }?;
+    let udp = ptr_at::<UdpHdr>(ctx, EthHdr::LEN + Ipv4Hdr::LEN).unwrap();
+    let dst_port = u16::from_be(unsafe { (*udp).dest });
+    let src_port = u16::from_be(unsafe { (*udp).source });
 
-    let dst_port = u16::from_be(unsafe { ptr::read_unaligned(ptr::addr_of!((*udp).dest)) });
-
-    info!(ctx, "UDP packet detected, port: {}", dst_port);
-
-    Ok(dst_port == 7777)
+    Ok(src_port == 7777 || dst_port == 7777)
 }
 
 #[inline(always)]
-unsafe fn ptr_at<T>(ctx: &XdpContext, offset: usize) -> Result<&T, ()> {
+fn ptr_at<T>(ctx: &XdpContext, offset: usize) -> Result<*const T, u32> {
     let start = ctx.data();
     let end = ctx.data_end();
 
     let len = mem::size_of::<T>();
 
     if start + offset + len > end {
-        return Err(());
+        return Err(xdp_action::XDP_ABORTED);
     }
-    let ptr = (start + offset) as *const T;
-
-    Ok(&*ptr)
+    Ok((start + offset) as *const T)
 }
 
 #[panic_handler]
