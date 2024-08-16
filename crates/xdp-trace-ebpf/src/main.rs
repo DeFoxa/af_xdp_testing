def734d0a47f27fc859a0f5c79526d91c8207685
DeFoxa <113225642+DeFoxa@users.noreply.github.com>
2024-08-16 11:25:32 -0500

    should_process_packets, try_should_process, ptr_at

--- crates/xdp-trace-ebpf/src/main.rs
+++ crates/xdp-trace-ebpf/src/main.rs
@@ -9,7 +9,12 @@ use aya_ebpf::{
     programs::XdpContext,
 };
 use aya_log_ebpf::info;
-use core::panic::PanicInfo;
+use core::{mem, panic::PanicInfo, ptr};
+use network_types::{
+    eth::{EthHdr, EtherType},
+    ip::{IpProto, Ipv4Hdr},
+    udp::UdpHdr,
+};
 
 #[derive(Debug, Copy, Clone)]
 #[repr(C)]
@@ -31,19 +36,63 @@ pub fn xdp_tracer(ctx: XdpContext) -> u32 {
 }
 
 fn try_xdp_tracer(ctx: XdpContext) -> Result<u32, u32> {
-    let event = XdpEvent {
-        timestamp: unsafe { bpf_ktime_get_ns() },
-        packet_size: (ctx.data_end() - ctx.data()) as u32,
-        action: xdp_action::XDP_PASS,
-    };
+    if should_process_packet(&ctx) {
+        let event = XdpEvent {
+            timestamp: unsafe { bpf_ktime_get_ns() },
+            packet_size: (ctx.data_end() - ctx.data()) as u32,
+            action: xdp_action::XDP_PASS,
+        };
 
-    unsafe {
-        EVENTS.output(&ctx, &event, 0);
+        unsafe {
+            EVENTS.output(&ctx, &event, 0);
+        }
+
+        info!(&ctx, "XDP event logged");
     }
 
-    info!(&ctx, "XDP event logged");
-
     Ok(xdp_action::XDP_PASS)
+}
+
+fn should_process_packet(ctx: &XdpContext) -> bool {
+    match try_should_process_packet(ctx) {
+        Ok(should_process) => should_process,
+        Err(_) => false,
+    }
+}
+fn try_should_process_packet(ctx: &XdpContext) -> Result<bool, ()> {
+    let eth = unsafe { ptr_at::<EthHdr>(ctx, 0) }?;
+
+    let ether_type = unsafe { ptr::read_unaligned(ptr::addr_of!((*eth).ether_type)) };
+    if ether_type != EtherType::Ipv4 {
+        return Ok(false);
+    }
+
+    let ip = unsafe { ptr_at::<Ipv4Hdr>(ctx, EthHdr::LEN) }?;
+
+    if ip.proto != IpProto::Udp {
+        return Ok(false);
+    }
+
+    let udp = unsafe { ptr_at::<UdpHdr>(ctx, EthHdr::LEN + Ipv4Hdr::LEN) }?;
+
+    let dst_port = u16::from_be(udp.dest);
+
+    Ok(dst_port == 7777)
+}
+
+#[inline(always)]
+unsafe fn ptr_at<T>(ctx: &XdpContext, offset: usize) -> Result<&T, ()> {
+    let start = ctx.data();
+    let end = ctx.data_end();
+
+    let len = mem::size_of::<T>();
+
+    if start + offset + len > end {
+        return Err(());
+    }
+    let ptr = (start + offset) as *const T;
+
+    Ok(&*ptr)
 }
 
 #[panic_handler]
